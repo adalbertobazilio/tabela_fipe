0d0ea96028cea3014c6fa1f591a79313c72c30b2
conato@gigasistemas.com.br <GigaSistemas>
2021-10-27 22:26:32 -0300

    Puxar Marcas de Carros

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,8 +10,43 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage> {
+
+
   String tipobusca = 'carro';
+  String Marca = '1';
+  String Modelo = '';
+  String Ano = '';
+  List ListaMarcas = [];
+
+
+  Future<String> pegarMarcas() async {
+    var url = Uri.parse('');
+    if (tipobusca == 'carro') {
+      url = Uri.parse('https://parallelum.com.br/fipe/api/v1/carros/marcas');
+    } else {
+      url = Uri.parse('https://parallelum.com.br/fipe/api/v1/motos/marcas');
+    }
+    var res = await http
+        .get(url);
+    var resBody = json.decode(res.body);
+
+    setState(() {
+      ListaMarcas = resBody;
+    });
+
+    //print(resBody);
+
+    return "Sucess";
+  }
+
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +56,7 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -53,29 +91,43 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(width: 10),
               ],
             ),
-          DropdownButton(
-            isExpanded: true,
-            value: null,
-            iconSize: 30,
-            items: null,
-            onChanged: null,
-            hint: Text('Selecione a Marca'),
-          ),
+            FutureBuilder(
+                future: pegarMarcas(),
+                builder: (context, snapshot) {
+                  return DropdownButton<String>(
+                      hint: Text("Selecione a Marca"),
+                      value: Marca,
+                      items: ListaMarcas.map((item) {
+                        return  DropdownMenuItem(
+                          child:  Text(item['nome']),
+                          value: item['codigo'].toString(),
+                        );
+                      }).toList(),
+
+                    onChanged: (value) {
+                      setState(() {
+                        Marca = value as String;
+                      });
+                    },
+
+                  );
+                }),
+
             DropdownButton(
               isExpanded: true,
-              value: null,
+              value: Modelo,
               iconSize: 30,
-              items: null,
-              onChanged: null,
               hint: Text('Selecione o Modelo'),
+              onChanged: null,
+              items: null,
             ),
             DropdownButton(
               isExpanded: true,
-              value: null,
+              value: Ano,
               iconSize: 30,
-              items: null,
-              onChanged: null,
               hint: Text('Selecione o Ano'),
+              onChanged: null,
+              items: null,
             )
           ],
         ),
